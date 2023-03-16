41fff2f9a4dff4d9fbe76f8c87b9753b258bb803
Denis Žižka <zizkade20@sps-prosek.cz>
2023-03-16 15:52:50 +0100

    Přidejte soubory projektu.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mujLabel.Content = i;
+        }
+
+       
+        private void Odeslat_Click(object sender, RoutedEventArgs e)
+        {
+            i++;
+            mujLabel.Content = i;
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            i++;
+            mujLabel.Content = i;
+            progress.Value = i;
+        }
+
+        
+    }
+}
