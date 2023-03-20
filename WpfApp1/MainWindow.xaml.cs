35794cccf5882e65e1397b3c8effdb1e3a3f0fd5
Denis Žižka <zizkade20@sps-prosek.cz>
2023-03-20 08:30:20 +0100

    get set

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,60 +16,55 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
+    class Variables
+    {
+        public static string spravnyVysledek { get; set; }
+        public static int level { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
-        int level = 1;
+        
         Random random = new Random();
-
-        private int answer;
 
 
         public MainWindow()
         {
             InitializeComponent();
-            LevelLabel.Content = "Level " + level;
 
-            if (level <= 3)
-            {
-                List<int> numbers = new List<int>() { 0, 2, 3, 4, 5 };
+            Variables.level = 1;
+            Variables.spravnyVysledek = "";
 
-                int randomNumber1 = numbers[random.Next(numbers.Count)];
-                int randomNumber2 = numbers[random.Next(numbers.Count)];
+            LevelLabel.Content = "Level " + Variables.level;
 
-                DisplayMultiplicationProblem(randomNumber1, randomNumber2);
+            ShowNumbers(Variables.level);
 
-            } else if (level <= 6)
-            {
-                List<int> numbers = new List<int>() { 6, 7, 8, 9, 10 };
-
-                int randomNumber1 = numbers[random.Next(numbers.Count)];
-                int randomNumber2 = numbers[random.Next(numbers.Count)];
-
-                DisplayMultiplicationProblem(randomNumber1, randomNumber2);
-            } else
-            {
-                List<int> numbers = new List<int>() { 12, 15, 16, 18, 19 };
-
-                int randomNumber1 = numbers[random.Next(numbers.Count)];
-                int randomNumber2 = numbers[random.Next(numbers.Count)];
-                
-
-                DisplayMultiplicationProblem(randomNumber1, randomNumber2);
-            }
-            
-
-
-            
-            
 
         }
 
-        private void Odeslat_ClickTrue(object sender, RoutedEventArgs e)
+        private void Odeslat_Click(object sender, RoutedEventArgs e)
         {
 
-            CheckAnswer(true);
+            string ButtonVal;
+            ButtonVal = (sender as System.Windows.Controls.Button).Content.ToString();
+
+            if (Variables.spravnyVysledek == ButtonVal)
+            {
+                Variables.level += 1;
+                ShowNumbers(Variables.level);
+            }
+            else
+            {
+                Variables.level = 1;
+                ShowNumbers(Variables.level);
+
+            }
 
 
+        }
+
+        private void ShowNumbers(int level)
+        {
             if (level <= 3)
             {
                 List<int> numbers = new List<int>() { 2, 3, 4, 5, 6 };
@@ -96,62 +91,29 @@ namespace WpfApp1
                 int randomNumber1 = numbers[random.Next(numbers.Count)];
                 int randomNumber2 = numbers[random.Next(numbers.Count)];
 
-
                 DisplayMultiplicationProblem(randomNumber1, randomNumber2);
             }
         }
-        private void Odeslat_ClickFalse(object sender, RoutedEventArgs e)
-        {
-            CheckAnswer(false);
-            List<int> numbers = new List<int>() { 2, 3, 4, 5, 6 };
-
-            int randomNumber1 = numbers[random.Next(numbers.Count)];
-            int randomNumber2 = numbers[random.Next(numbers.Count)];
-
-            DisplayMultiplicationProblem(randomNumber1, randomNumber2);
-        }
-
-
-
-
 
         private void DisplayMultiplicationProblem(int x, int y)
         {
 
-            int answer = x * y;
+            int SpravnyVysledek = x * y;
             PrikladLabel.Content = $"kolik je {x} krát {y}?";
-            /*
-            int correctAnswerButton = random.Next(2);
-            if (correctAnswerButton == 0)
+
+            int randomiseButton = random.Next(2);
+            if (randomiseButton == 0)
             {
-                Odpoved1.Content = answer;
-                Odpoved2.Content = answer + x;
+                Odpoved1.Content = SpravnyVysledek;
+                Odpoved2.Content = SpravnyVysledek + x;
             }
             else
             {
-                Odpoved2.Content = answer;
-                Odpoved1.Content = answer + x;
+                Odpoved2.Content = SpravnyVysledek;
+                Odpoved1.Content = SpravnyVysledek + x;
             }
-            */
-            Odpoved1.Content = answer;
-            Odpoved2.Content = answer + x;
+            Variables.spravnyVysledek += SpravnyVysledek;
         }
-        
 
-        private void CheckAnswer(bool answer)
-        {
-            if (answer)
-            {
-                level++;
-                LevelLabel.Content = "Level " + level;
-            }
-            else
-            {
-                level = 1;
-                LevelLabel.Content = "Level " + level;
-
-            }
-        }
-        
     }
 }
